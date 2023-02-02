62a947779764d4a316e56fa28a4da3841d01bf00
soufaker <44189635+Soufaker@users.noreply.github.com>
2023-02-02 11:41:15 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 git clone https://github.com/maurosoria/dirsearch.git ./inifile/dirsearch-master
+chmod 777 ./inifile/lousao/nuclei
 yum -y install bind-utils
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
+
 
