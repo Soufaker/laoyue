9da8759305be1263620b8712a5721d9204ee2b43
soufaker <44189635+Soufaker@users.noreply.github.com>
2023-03-23 21:20:01 +0800

    update

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 git clone https://github.com/maurosoria/dirsearch.git ./inifile/dirsearch-master
 chmod 777 ./inifile/lousao/nuclei
+chmod 777 ./inifile/naabu/naabu
+chmod 777 ./inifile/httpx/httpx
 #cp ./inifile/lousao/nuclei-templates/ /root/
 #sudo apt-get install yum
-yum install libpcap-devel
-yum  -y install mlocate | updatedb
-ln -s libpcap.so.1.5.3 libpcap.so.0.8
-yum -y install bind-utils
+#yum  -y install mlocate;updatedb
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+yum -y install bind-utils
+yum install libpcap-devel
+cd /usr/lib64/;sudo ln -s libpcap.so.1.5.3 libpcap.so.0.8
+
 
 
