db9e65f6a0c2153f6c76fa40a1a98a2f4d4ddc88
Soufaker <44189635+Soufaker@users.noreply.github.com>
2023-09-20 16:52:37 +0800

    Update laoyue.py

--- laoyue.py
+++ laoyue.py
@@ -1346,5 +1346,5 @@ if __name__ == '__main__':
             print('发送消息异常')
             os.system('nohup python3 laoyue.py  -d "SRC.txt" -z -f -n -m &')
     if notauto != True:
-        time.sleep(3600)
+        time.sleep(360)
         os.system('nohup python3 laoyue.py  -d "SRC.txt" -z  -n -f -m &')
