558499a9adea3eb93ec16e58cbcc6c97e5f2d0cd
Soufaker <44189635+Soufaker@users.noreply.github.com>
2023-12-20 12:05:51 +0800

    update

--- laoyue.py
+++ laoyue.py
@@ -1329,7 +1329,7 @@ if __name__ == '__main__':
     # 调用fofa,yt获取信息
     if args.help:
         check_msg = ''
-        now_version = 'v1.2.0'
+        now_version = 'v1.2.1'
         new_version = get_latest_release_version()
         if now_version == new_version:
             check_msg = '现在是最新版本'
