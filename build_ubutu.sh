31f55e5cc1b65e6ec999aca6ea86c19e8e1e4de1
Soufaker <44189635+Soufaker@users.noreply.github.com>
2024-01-05 09:26:38 +0800

    Update build_ubutu.sh

--- build_ubutu.sh
+++ build_ubutu.sh
@@ -9,11 +9,9 @@ sed -i "s/\r//" check_nohup_size.sh
 chmod 777 check_nohup_size.sh
 systemctl enable docker.service
 chmod 777 ./inifile/bypass403/f403_linux_amd64
-#cp ./inifile/lousao/nuclei-templates/ /root/
-#sudo apt-get install yum
-#yum  -y install mlocate;updatedb
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-yum -y install bind-utils
-yum install libpcap-devel
-cd /usr/lib64/;sudo ln -s libpcap.so.1.5.3 libpcap.so.0.8
+sudo apt-get update
+sudo apt-get install python3-pip
+pip3 install -r requirements.txt
+sudo apt-get install dnsutils
+sudo apt-get install libpcap-dev
+sudo ln -s /usr/lib/x86_64-linux-gnu/libpcap.so.1 /usr/lib/x86_64-linux-gnu/libpcap.so.0.8
