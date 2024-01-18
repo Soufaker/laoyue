f55344813fb1474e6e6257d82e352166a6bbff48
Soufaker <44189635+Soufaker@users.noreply.github.com>
2024-01-18 17:14:34 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 git checkout -- config/config.ini
 git checkout -- SRC.txt
+git checkout -- caches/sm_cache.txt
+git checkout -- result/awvslist/cache.txt
+git checkout -- inifile/lousao/nuclei
 git pull
